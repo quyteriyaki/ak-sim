3b9b913fbb74371f55f89e1d3d6d198f1376c401
quyteriyaki <quyteriyaki@gmail.com>
2020-04-05 17:52:50 +1000

    Fixed several skills not replaced

--- tools/separate_character.py
+++ tools/separate_character.py
@@ -11,8 +11,9 @@ with open('./parsed_src/skill_table.json', 'r') as file:
 for character in characters:
     with open('./parsed_src/op_src/'+character+'.json', 'w') as file:
         if 'skills' in characters[character]:
-            for skill in characters[character]['skills']:
+            list_skills = characters[character]['skills']
+            characters[character]['skills'] = []
+            for skill in list_skills:
                 if str(skill) in skills:
-                    characters[character]['skills'].remove(skill)
                     characters[character]['skills'].append(skills[skill])
         file.write(json.dumps(characters[character], indent=2))
