f965dcd518b37e854381cc128d0d924af2685e51
quyteriyaki <quyteriyaki@gmail.com>
2020-04-07 17:56:39 +1000

    Added / Changed History preview in Timeline

--- tools/Timeline.py
+++ tools/Timeline.py
@@ -25,14 +25,40 @@ class Timeline:
             return super().__getattribute__(name)
     
     def getNext(self, time, step = 1):
-        e = (time, self._keys[time])
-        i = self.keys.index(e) + step
-        return self.keys[i][0]
+        if time in self._keys:
+            e = (time, self._keys[time])
+            i = self.keys.index(e) + step
+            return self.keys[i]
+        else:
+            # Needs to be adjusted
+            e = (0,self._keys[0])
+            for i in self.keys:
+                if e[0] - time < i[0] - time: e = i
+                if e[0] - time > 0: break
+            i = self.keys.index(e) + step - 1
+            if i > len(self.keys) - 1:
+                return self.keys[i - 1]
+            return self.keys[i]
+        
     
     def getPrev(self, time, step = 1):
-        e = (time, self._keys[time])
-        i = self.keys.index(e) - step
-        return self.keys[i][0]
+        if time in self._keys:
+            e = (time, self._keys[time])
+            i = self.keys.index(e) - step
+            return self.keys[i]
+        else:
+            # Needs to be adjusted
+            e = (0,self._keys[0])
+            for i in self.keys:
+                if time - e[0] < 0: break
+                if time - e[0] > time - i[0] : e = i
+            i = self.keys.index(e) - step
+            if i > 0:
+                return self.keys[0]
+            return self.keys[i]       
+    
+    def getLatest(self):
+        return self.keys[-1]
     
     def pushKey(self, time, step):
         if time in self._keys:
@@ -47,3 +73,37 @@ class Timeline:
             for x in v:
                 self.add(nTime, x)
         del self._keys[time]
+
+    def rmLastCmd(self, cmd):
+        c = len(self._keys) - 1
+        while c >= 0:
+            if cmd in self.keys[c][1]:
+                self._keys[self.keys[c][0]].remove(cmd)
+                return 1
+            c -= 1
+        return 0
+    
+    def getNextCmd(self, time, cmd):
+        if time in self._keys:
+            v = self.keys.index((time,self._keys[time])) + 1
+            while v < len(self._keys):
+                if cmd in self.keys[v][1]:
+                    return self.keys[v]
+                v += 1
+        return 0
+    
+    def getPrevCmd(self, time, cmd):
+        if time in self._keys:
+            v = self.keys.index((time,self._keys[time])) - 1
+            while  v > 0:
+                if cmd in self.keys[v][1]:
+                    return self.keys[v]
+                v -= 1
+        return 0
+    
+    def rmCmd(self, time, cmd):
+        if time in self._keys:
+            self._keys[time].remove(cmd)
+
+            if self._keys[time] == []:
+                self.remove(time)
