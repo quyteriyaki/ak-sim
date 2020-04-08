05d96f8d39c1d6466140f82815b26fd8711445a6
quyteriyaki <quyteriyaki@gmail.com>
2020-04-08 20:49:51 +1000

    Condensed Timeline

--- tools/Timeline.py
+++ tools/Timeline.py
@@ -2,7 +2,7 @@ class Timeline:
     def __init__ (self):
         self._keys = {}
     
-    def add(self, time, *cmds):
+    def addKey(self, time, *cmds):
         if time in self._keys:
             for cmd in cmds:
                 if cmd not in self._keys[time]: self._keys[time].append(cmd)
@@ -11,26 +11,21 @@ class Timeline:
             for cmd in cmds:
                 self._keys[time].append(cmd)
     
-    def remove(self, time):
+    def rmKey(self, time):
         if time in self._keys:
             del self._keys[time]
     
     def __getattribute__(self,name):
         if name == "keys":
             return sorted(self._keys.items(), key=lambda item: item[0])
-        elif name == "head":
-            if len(self._keys) > 0:
-                return self.keys[0]
-            else:
-                return 0
-        else:
-            return super().__getattribute__(name)
+        if name == "latest":
+            return self.keys[-1]
+        return super().__getattribute__(name)
     
     def getNext(self, time, step = 1):
         if time in self._keys:
             e = (time, self._keys[time])
-            i = self.keys.index(e) + step
-            return self.keys[i]
+            return self.keys[self.keys.index(e) + step]
         else:
             # Needs to be adjusted
             e = (0,self._keys[0])
@@ -41,13 +36,11 @@ class Timeline:
             if i > len(self.keys) - 1:
                 return self.keys[i - 1]
             return self.keys[i]
-        
     
     def getPrev(self, time, step = 1):
         if time in self._keys:
             e = (time, self._keys[time])
-            i = self.keys.index(e) - step
-            return self.keys[i]
+            return self.keys[self.keys.index(e) - step]
         else:
             # Needs to be adjusted
             e = (0,self._keys[0])
@@ -59,21 +52,11 @@ class Timeline:
                 return self.keys[0]
             return self.keys[i]       
     
-    def getLatest(self):
-        return self.keys[-1]
-    
-    def pushKey(self, time, step):
-        if time in self._keys:
-            v = self._keys[time]
-            for x in v:
-                self.add(time + step, x)
-        del self._keys[time]
-    
     def moveKey(self, time, nTime):
         if time in self._keys:
             v = self._keys[time]
             for x in v:
-                self.add(nTime, x)
+                self.addKey(nTime, x)
         del self._keys[time]
 
     def rmLastCmd(self, cmd):
@@ -106,6 +89,5 @@ class Timeline:
     def rmCmd(self, time, cmd):
         if time in self._keys:
             self._keys[time].remove(cmd)
-
             if self._keys[time] == []:
                 self.remove(time)
